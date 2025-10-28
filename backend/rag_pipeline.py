a0278b9e82dd0bc5ac909e9e6305375a6c366a86
AviralMishra03 <aviral.mishra0309@gmail.com>
2025-10-28 16:46:20 +0530

    twentycommit

--- backend/rag_pipeline.py
+++ backend/rag_pipeline.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import DirectoryLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitters import RecursiveCharacterTextSplitter
 # --- Import LOCAL Embedding Library ---
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
